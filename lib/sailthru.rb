9e595bd795c40c1e585d228583e988d6c5e6c17e
Kevin Urrutia <urrutia.kevin@gmail.com>
2013-07-23 15:22:27 -0700

    fix space v4

--- lib/sailthru.rb
+++ lib/sailthru.rb
@@ -660,7 +660,7 @@ module Sailthru
       data['emails'] = Array(emails).join(',')
       process_job(:import, data, report_email, postback_url)
     end
-    
+
     # implementation for import job using file upload
     def process_import_job_from_file(list, file_path, report_email = nil, postback_url = nil)
       data = {}
@@ -668,7 +668,7 @@ module Sailthru
       data['file'] = file_path
       process_job(:import, data, report_email, postback_url, 'file')
     end
-    
+
     # implementation for update job using file upload
     def process_update_job_from_file(file_path, report_email = nil, postback_url = nil)
       data = {}
