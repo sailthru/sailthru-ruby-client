fa164eedc03736721f544f62557ac5ae6e8b045d
Mike Perham <mperham@gmail.com>
2014-06-17 09:05:39 -0700

    Rdoc

--- lib/sailthru/client.rb
+++ lib/sailthru/client.rb
@@ -13,6 +13,14 @@ module Sailthru
   class UnavailableError < StandardError
   end
 
+  # Provides a global place to configure the credentials for an application.
+  # For instance, in your Rails app, create +config/initializers/sailthru.rb+
+  # and place this line in it:
+  #
+  #     Sailthru.credentials('apikey', 'secret')
+  #
+  # Now you can create a client instance easily via Sailthru::Client.new
+  #
   def self.credentials(api_key, secret)
     @api_key = api_key
     @secret = secret
