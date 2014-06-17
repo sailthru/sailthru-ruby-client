5bcde26aeed904b20d30a7749143d32cb2397e60
Mike Perham <mperham@gmail.com>
2014-06-17 12:33:19 -0700

    Move Sailthru API into sailthru.rb

--- lib/sailthru.rb
+++ lib/sailthru.rb
@@ -1,2 +1,31 @@
 require 'sailthru/version'
 require 'sailthru/client'
+
+module Sailthru
+  class ClientError < StandardError
+  end
+
+  class UnavailableError < StandardError
+  end
+
+  # Provides a global place to configure the credentials for an application.
+  # For instance, in your Rails app, create +config/initializers/sailthru.rb+
+  # and place this line in it:
+  #
+  #     Sailthru.credentials('apikey', 'secret')
+  #
+  # Now you can create a client instance easily via Sailthru::Client.new
+  #
+  def self.credentials(api_key, secret)
+    @api_key = api_key
+    @secret = secret
+  end
+
+  def self.api_key
+    @api_key
+  end
+
+  def self.secret
+    @secret
+  end
+end
--- lib/sailthru/client.rb
+++ lib/sailthru/client.rb
@@ -7,33 +7,6 @@ require 'cgi'
 require 'json'
 
 module Sailthru
-  class ClientError < StandardError
-  end
-
-  class UnavailableError < StandardError
-  end
-
-  # Provides a global place to configure the credentials for an application.
-  # For instance, in your Rails app, create +config/initializers/sailthru.rb+
-  # and place this line in it:
-  #
-  #     Sailthru.credentials('apikey', 'secret')
-  #
-  # Now you can create a client instance easily via Sailthru::Client.new
-  #
-  def self.credentials(api_key, secret)
-    @api_key = api_key
-    @secret = secret
-  end
-
-  def self.api_key
-    @api_key
-  end
-
-  def self.secret
-    @secret
-  end
-
   class Client
     DEFAULT_API_URI = 'https://api.sailthru.com'
 
