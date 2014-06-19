8888e29b807f3dc7abdf70c3059704eafc9f3903
Ivan Tse <ivan.tse1@gmail.com>
2014-06-19 15:08:50 -0400

    Remove controller key correctly

--- lib/sailthru/client.rb
+++ lib/sailthru/client.rb
@@ -279,7 +279,7 @@ module Sailthru
         return false unless params[:action] == :verify
 
         sig = params.delete(:sig)
-        sig = sig.delete_if {|key, value| key == :controller}
+        params.delete(:controller)
         return false unless sig == get_signature_hash(params, @secret)
 
         _send = get_send(params[:send_id])
@@ -305,7 +305,7 @@ module Sailthru
         return false unless params[:action] == 'optout'
 
         sig = params.delete(:sig)
-        sig = sig.delete_if {|key, value| key == :controller}
+        params.delete(:controller)
         return false unless sig == get_signature_hash(params, @secret)
         return true
       else
@@ -325,7 +325,7 @@ module Sailthru
         return false unless params[:action] == 'hardbounce'
 
         sig = params.delete(:sig)
-        sig = sig.delete_if {|key, value| key == :controller}
+        params.delete(:controller)
         return false unless sig == get_signature_hash(params, @secret)
         return true
       else
